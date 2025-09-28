3f6d8b645f9e1821105f8b009dedc4a4a3e9cd38
g33knation <g33knation1@gmail.com>
2025-09-27 23:37:47 -0700

    Delete init.sql

--- init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS events (
-    id SERIAL PRIMARY KEY,
-    source TEXT NOT NULL,
-    type TEXT NOT NULL,
-    payload JSONB NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
